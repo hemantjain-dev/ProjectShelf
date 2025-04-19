e687c496b2ac03ca9bd26ed4df49705bee3b1e0c
hemantjain-dev <hemantjain.dev@gmail.com>
2025-04-19 17:37:03 +0530

    cors set to default

--- server/src/app.js
+++ server/src/app.js
@@ -32,13 +32,13 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet())
-app.use(cors({
-    origin: ['https://projectshelfs.vercel.app', 'http://localhost:5173'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
-}));
-//app.use(cors()); // https://projectshelfs.vercel.app/
+// app.use(cors({
+//     origin: ['https://projectshelfs.vercel.app', 'http://localhost:5173'],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+// }));
+app.use(cors()); // https://projectshelfs.vercel.app/
 app.use(cookieParser());
 app.use(morgan('dev'));
 
